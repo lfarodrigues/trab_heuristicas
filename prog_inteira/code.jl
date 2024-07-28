4f1810c479ed590341f4bbca52c5ef5d61ea9ced
Mino <dev@mino.one>
2024-07-28 15:12:18 -0300

    Prog inteira pronta

--- prog_inteira/code.jl
+++ prog_inteira/code.jl
@@ -1,3 +1,4 @@
+# Trabalho Prático - Otimização combinatória
 import Pkg
 
 Pkg.activate(@__DIR__)
@@ -8,6 +9,7 @@ Pkg.instantiate()
 
 using JuMP
 using HiGHS
+using Random
 
 # Instancia Uma instancia é composta por uma unica capacidade c, n itens, e m
 # arestas.
@@ -20,15 +22,19 @@ using HiGHS
 # mochila pode ter nenhum dos dois itens, ou s´o o item i, ou s´o o item
 # j, mas nao os itens i e j).
 
+# Executa o modelo do problema da mochila com grafo de conflito
+# Apenas printa na tela os resultados
 function knapsack_problem(
-	c::Int,
-	n::Int,
+	n::Int,	
+	c::Int,	
 	p::Vector{Int},
 	w::Vector{Int},
-	E::Vector{Tuple{Int, Int}}
+	E::Vector{Tuple{Int, Int}},
+	time_limit_sec::Int,
 )
 	# Constrói o modelo.
 	model = Model(HiGHS.Optimizer)
+	set_time_limit_sec(model, time_limit_sec)
 
 	@variable(model, escolha[1:n], Bin)
 	@objective(model, Max, p' * escolha)
@@ -39,13 +45,140 @@ function knapsack_problem(
 
 	@show value.(escolha)
 	@show objective_value(model)
+	@show value(sum(w' * escolha))
 end
 
-c = 9
-n = 10
+# Função auxiliar para ler um int do arquivo de input
+function parse_int(prefix::String, value::String)
+	return parse(Int, strip(value[length(prefix)+1:length(value)-1]))
+end
 
-p = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-w = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-E = [(1, 2), (3, 4)]
+# Função auxiliar para ler uma tupla de valores do arquivo de input
+function parse_tuple(value::String)
+	S = split(strip(value), " ")
+	T = []
+	for p in S
+		# as tuplas estão tabuladas no arquivo
+		# isso causa diversos espaços em branco a mais no input
+		isempty(p) && continue
+		v = parse(Int, strip(p))
+		push!(T, v)
+	end
+	return Tuple(T)
+end
 
-knapsack_problem(c, n, p, w, E)
+# Parsa o arquivo de input
+# Retorna (n,c,p,w,E)
+function parse_file(filepath::String)
+	n = 0
+	c = 0
+	p = Int[]
+	w = Int[]
+	E = Tuple{Int,Int}[]
+
+	open(filepath) do f		
+			while !eof(f)	    	
+				line = readline(f)
+
+				if occursin("param n :=", line)
+					n = parse_int("param n :=", line)
+				end
+
+				if occursin("param c :=", line)
+					c = parse_int("param c :=", line)
+				end
+
+				if occursin("param : V : p w :=", line)
+					while true
+						line = readline(f)
+						occursin(";", line) && break
+						T = parse_tuple(line)
+						push!(p, T[2])
+						push!(w, T[3])
+					end
+				end				
+
+				if occursin("set E :=", line)
+					while true
+						line = readline(f)
+						occursin(";", line) && break
+						T = parse_tuple(line)
+						# Os índices estão 0-indexed
+						# O +1 é para fazer com que sejam 1-indexed
+						push!(E, (T[1] + 1, T[2] + 1))
+					end
+				end					
+			end
+	end
+
+	println("n: ", n)
+	println("c: ", c)
+	println("p: ", length(p))
+	println("w: ", length(w))
+	println("E: ", length(E))
+
+	return n, c, p, w, E
+end
+
+function print_usage()
+	println("")
+	println("Uso: julia code.jl <caminho> --seed=<semente> --limit=<limite (s)>")
+	println("Parâmetros com nome são opcionais")	
+end
+
+# Garante que tem pelo menos 1 argumento
+if length(ARGS) == 0
+	println("ERROR: Faltaram parâmetros")
+	print_usage()
+	exit()
+end
+
+# Parâmetro de ajuda
+if lowercase(ARGS[1]) == "-h" || lowercase(ARGS[1]) == "--help"
+	print_usage()
+	exit()
+end
+
+# Função auxiliar para parsar um int de um parâmetro nomeado
+function parse_arg_int(name::String, default::Any)
+	for arg in ARGS
+		occursin(name, arg) || continue
+		v = split(strip(arg), "=")
+		return parse(Int, v[length(v)])
+	end
+
+	return default
+end
+
+# Função auxiliar para buscar o parâmetro principal
+# Só pra garantir que a ordem que for passado as coisas não importa
+function first_positional_param_string()
+	for arg in ARGS
+		isempty(strip(arg)) && continue
+		startswith(strip(arg), "--") && continue
+		return String(strip(arg))
+	end
+
+	println("ERROR: Faltou parâmetro posicional")
+	print_usage()
+	exit()
+end
+
+# Recebe os inputs da linha de comando
+filepath = first_positional_param_string()
+random_seed = parse_arg_int("--seed", nothing)
+time_limit_sec = parse_arg_int("--limit", 3600)
+
+println("Caminho: ", filepath)
+println("Seed: ", random_seed)
+println("Limite: ", time_limit_sec)
+
+# Não tenho certeza se é aqui que devo setar a "semente de aleatoriedade"
+# Não encontrei o uso disso nos slides nem nada
+Random.seed!(random_seed)
+
+# Parsa o arquivo
+n, c, p, w, E = parse_file(filepath)
+
+# Executa o problema
+knapsack_problem(n, c, p, w, E, time_limit_sec)
